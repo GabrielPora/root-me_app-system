145874a1851858c3fa36ba84dbfe7693f8346c5c
GabrielPora <gabriel.groener@gmail.com>
2017-11-29 07:55:25 +0200

    updated file

--- stack_overflow.c
+++ stack_overflow.c
@@ -1,28 +1,27 @@
 #include <string.h>
 #include <stdio.h>
 
-void    foo(void)
+void	foo(void)
 {
-    char    buff[16] = {0};
-
-    buff[0] = 'A';
-    buff[1] = 'A';
-    buff[2] = 'A';
-    buff[3] = 'A';
-    (void)buff;
+	char    buff[16] = {0};	
+	buff[0] = 'A';
+	buff[1] = 'A';
+	buff[2] = 'A';
+	buff[3] = 'A';
+	(void)buff;
 }
 
-int main(int __attribute__((unused))ac, const char **av)
+int	main(int __attribute__((unused))ac, const char **av)
 {
-    int     value;
-    char    buff[16] = {0};
-
-    value = 0;
-    strcpy(buff, av[1]);
-    if (value != 0)
-        dprintf(1, "WIN %d\n", value);
-    else
-        dprintf(1, "Try again\n");
-    foo();
-    return (0);
+	int		value;
+	char	buff[16] = {0};	
+	
+	value = 0;
+	strcpy(buff, av[1]);
+	if (value != 0)
+		dprintf(1, "WIN %d\n", value);
+	else
+		dprintf(1, "Try again\n");
+	foo();
+	return (0);
 }
